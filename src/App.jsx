714eccb32da4bfdad5dfd9142a693d8c03db9cbe
Albin Joby <albinjobyponnu@gmail.com>
2025-07-05 11:59:52 +0530

    render tiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { use, useEffect, useState } from "react";
 
 const API_URL = "/api/api/fe/wordle-words";
+const WORD_LENGTH = 5
 
 export default function App() {
   const [solution, setSolution] = useState("");
+  const [guesses, setGuesses] = useState(Array(6).fill(null));
 
   useEffect(() => {
     const fetchWord = async () => {
@@ -15,5 +17,25 @@ export default function App() {
     fetchWord();
   }, []);
 
-  return <main className="h-screen flex-center">{solution}</main>;
+  return (
+  <main className="h-screen flex-center flex-col gap-1">
+    {guesses.map(guess => {
+      return <Line guess={guess ?? ''}/>
+    })}
+    {solution}
+  </main>
+  );
+}
+
+function Line(guess){
+  const tiles = []
+  for (let i = 0; i < WORD_LENGTH; i++) {
+    const char = guess[i]
+    tiles.push(<div key={i} className="tile">{char}</div>)
+  }
+  return(
+    <div className="flex gap-1">
+      {tiles}
+    </div>
+  )
 }
