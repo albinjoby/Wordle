c0f7d9922eb7542a9b550111fe871ddd60e00081
Albin Joby <albinjobyponnu@gmail.com>
2025-07-05 11:48:36 +0530

    fetch solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { use, useEffect, useState } from "react";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+const API_URL = "/api/api/fe/wordle-words";
+
+export default function App() {
+  const [solution, setSolution] = useState("");
+
+  useEffect(() => {
+    const fetchWord = async () => {
+      const response = await fetch(API_URL);
+      const words = await response.json();
+      const randomWord = words[Math.floor(Math.random() * words.length)];
+      setSolution(randomWord);
+    };
+    fetchWord();
+  }, []);
+
+  return <main className="h-screen flex-center">{solution}</main>;
 }
-
-export default App
